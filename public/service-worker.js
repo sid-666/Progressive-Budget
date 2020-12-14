45b5f27d668273e9f4cd5d44ae26273d1bc8192b
Seance <siddharth.dhananjay@gmail.com>
2020-12-15 10:56:00 +1100

    defined cache name and data cahce name

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,4 +5,7 @@ const FILES_TO_CACHE = [
     '/index.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-  ];
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
