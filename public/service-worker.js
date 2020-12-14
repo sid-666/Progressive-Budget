4973d0e737ebc37985793eda25bd13b3e4c13a34
Seance <siddharth.dhananjay@gmail.com>
2020-12-15 10:57:14 +1100

    defined install event listener for pre-caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,3 +9,15 @@ const FILES_TO_CACHE = [
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
